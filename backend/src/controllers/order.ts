d8a595362a403ed66f0f34b7c9f5130a03f5cc7a
Dário Vasconcelos <30694257+dariogsv@users.noreply.github.com>
2023-12-18 02:25:19 -0300

    feat: order crud  with controllers, routes, collection, repository, repositoryInterface and model

---
+++ backend/src/controllers/order.ts
@@ -0,0 +1,82 @@
+import { OrderCollection } from "../data/order"
+import Order from "../models/order"
+
+export class OrderRegisterController{
+    private orderCollection: OrderCollection;
+
+    constructor(orderCollection: OrderCollection){
+        this.orderCollection = orderCollection;
+    }
+    
+    async create(req: any, res: any) {
+        const order = await this.orderCollection.create(req.body)
+        res.send(order)
+    }
+}
+
+export class OrderListController{
+    private orderCollection: OrderCollection;
+
+    constructor(orderCollection: OrderCollection){
+        this.orderCollection = orderCollection;
+    }
+    
+    async list(req: any, res: any) {
+        const user = req.body
+        console.log(`Listando orders para o usuário ${user.id}...`)
+        
+        var orders: Order[] | null
+        
+        if (['doorman', 'admin'].includes(user.type)){
+            orders = await this.orderCollection.listAll()
+        }
+        else{
+            orders = await this.orderCollection.listByUserID(user.id)
+        }
+        res.send(orders)
+    }
+}
+
+export class OrderDetailsController{
+    private orderCollection: OrderCollection;
+    
+    constructor(orderCollection: OrderCollection){
+        this.orderCollection = orderCollection;
+    }
+    
+    async get(req: any, res: any) {
+        // console.log(req)
+        const orderID = req.query.id
+        console.log(`Exibindo order ${orderID}...`)
+        const order = await this.orderCollection.read(orderID)
+        res.send(order)
+    }
+}
+
+export class OrderArrivedController{
+    private orderCollection: OrderCollection;
+
+    constructor(orderCollection: OrderCollection){
+        this.orderCollection = orderCollection;
+    }
+    
+    async arrived(req: any, res: any) {
+        const orderID = req.query.id
+        const order = await this.orderCollection.update({id: orderID, status: "arrived"})
+        res.send(order)
+    }
+}
+
+export class OrderCancelController{
+    private orderCollection: OrderCollection;
+
+    constructor(orderCollection: OrderCollection){
+        this.orderCollection = orderCollection;
+    }
+    
+    async cancel(req: any, res: any) {
+        const orderID = req.query.id
+        const order = await this.orderCollection.delete(orderID)
+        res.send(order)
+    }
+}
---
+++ backend/src/data/order.ts
@@ -0,0 +1,154 @@
+import Order from "../models/order";
+import sqlite3 from 'sqlite3';
+
+interface IOrderRepository {
+    create(order: Partial<Order>): Promise<Order>;
+    read(id: string): Promise<Order | null>;
+    listByUserID(userID: string): Promise<Order[] | null>;
+    listAll(): Promise<Order[] | null>;
+    update(order: Partial<Order>): Promise<Order>;
+    delete(id: string): Promise<Order>;
+}
+
+class OrderRepositoryBDR implements IOrderRepository {
+    db = new sqlite3.Database('./src/data/database.sqlite', (error) => {
+        if (error) {
+            console.error(error.message);
+            throw error;
+        }
+        console.log('Conexão com o banco de dados estabelecida.');
+    });
+    doReadSql(sql: string, params: any[]): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.db.all(sql, params, (err, rows: Order[]) => {
+                if (err) {
+                    console.error(err.message);
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    }
+    doWriteSql(sql: string, params: any[]): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.db.get(sql, params, (err, row: Order) => {
+                if (err) {
+                    console.error(err.message);
+                    reject(err);
+                } else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    async create(order: Partial<Order>): Promise<Order> {
+        const columns = Object.keys(order).join(', ');
+        const placeholders = Object.keys(order).map(() => '?').join(', ');
+        const values = Object.values(order);
+
+        const sql = `INSERT INTO Orders (${columns}) VALUES (${placeholders}) RETURNING *`;
+        try {
+            const row = await this.doWriteSql(sql, values);
+            return row;
+        } catch (error: any) {
+            console.error(error.message);
+            throw error;
+        }
+    }
+
+    async read(id: string): Promise<Order | null> {
+        const sql = 'SELECT * FROM Orders WHERE id = ?';
+        const params = [id];
+        try {
+            const orders = await this.doReadSql(sql, params);
+            return orders[0] || null;
+        } catch (error: any) {
+            console.error(error.message);
+            return null;
+        }
+    }
+
+    async listByUserID(userID: string): Promise<Order[] | null> {
+        const sql = 'SELECT * FROM Orders WHERE userID = ?';
+        const params = [userID];
+        try {
+            const orders = await this.doReadSql(sql, params);
+            return orders || null;
+        } catch (error: any) {
+            console.error(error.message);
+            return null;
+        }
+    }
+
+    async listAll(): Promise<Order[] | null> {
+        const sql = 'SELECT * FROM Orders';
+        try {
+            const orders = await this.doReadSql(sql, []);
+            return orders || null;
+        } catch (error: any) {
+            console.error(error.message);
+            return null;
+        }
+    }
+    async update(order: Partial<Order>): Promise<Order> {
+        const orderID = order.id
+        delete order.id
+        
+        const columns = Object.keys(order)
+            .map(key => `${key} = ?`)
+            .join(', ');
+        const params = Object.values(order)
+        params.push(String(orderID))
+
+        const sql = `UPDATE Orders SET ${columns} WHERE id = ? RETURNING *`;
+        try {
+            const row = await this.doWriteSql(sql, params);
+            return row
+        } catch (error: any) {
+            console.error(error.message);
+            throw error;
+        }
+    }
+
+    async delete(id: string): Promise<Order> {
+        const sql = 'DELETE FROM Orders WHERE id = ? RETURNING *';
+        const params = [id];
+        try {
+            const row = await this.doWriteSql(sql, params);
+            return row
+        } catch (error: any) {
+            console.error(error.message);
+            throw error;
+        }
+    }
+
+}
+
+class OrderCollection {
+    private orderRepository: IOrderRepository;
+
+    constructor(orderRepository: IOrderRepository) {
+        this.orderRepository = orderRepository;
+    }
+    create(order: Order): Promise<Order> {
+        return this.orderRepository.create(order);
+    }
+    read(id: string): Promise<Order | null> {
+        return this.orderRepository.read(id);
+    }
+    listByUserID(userID: string): Promise<Order[] | null> {
+        return this.orderRepository.listByUserID(userID);
+    }
+    listAll(): Promise<Order[] | null> {
+        return this.orderRepository.listAll();
+    }
+    update(order: Partial<Order>): Promise<Order> {
+        return this.orderRepository.update(order);
+    }
+    delete(id: string): Promise<Order> {
+        return this.orderRepository.delete(id);
+    }
+}
+export { IOrderRepository, OrderRepositoryBDR, OrderCollection };
