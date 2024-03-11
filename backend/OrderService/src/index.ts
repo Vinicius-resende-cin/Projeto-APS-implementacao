a1129c478c220ce3a3ccbc9d2a3842db89a08864
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2024-03-11 13:19:05 -0300

    Order service register to discovery

--- backend/OrderService/src/index.ts
+++ backend/OrderService/src/index.ts
@@ -8,6 +8,23 @@ import { router } from "./routes/routes";
 
 app.use(express.json());
 app.use(cors());
+
+const registerServiceDiscovery = async () => {
+  const serviceName = "order";
+  const servicePort = 3101;
+
+  await fetch(`http://discovery:3001/?name=${serviceName}&port=${servicePort}`, {
+    method: "POST"
+  })
+    .then(() => {
+      console.log("Service registered");
+    })
+    .catch((err) => {
+      console.error("Error registering service", err);
+    });
+};
+registerServiceDiscovery();
+
 app.use(router);
 
 app.listen(3101, () => {
